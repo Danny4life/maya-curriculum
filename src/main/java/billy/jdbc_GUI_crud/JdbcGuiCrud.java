b0ce529d310cea8945650de661fec5e41da9222a
Danny4life <osiki.segun@gmail.com>
2025-03-04 23:00:47 +0100

    add files

---
+++ src/main/java/billy/jdbc_GUI_crud/JdbcGuiCrud.java
@@ -0,0 +1,14 @@
+package billy.jdbc_GUI_crud;
+
+public class JdbcGuiCrud {
+
+    /**
+     * CREATE DATABASE testdb;
+     * USE testdb;
+     * CREATE TABLE users (
+     *     id INT PRIMARY KEY AUTO_INCREMENT,
+     *     name VARCHAR(100),
+     *     age INT
+     * );
+     */
+}
