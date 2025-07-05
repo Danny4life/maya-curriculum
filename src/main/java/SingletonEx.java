4fa16fe6623ddbaa2adecd4b9f23944561d6d0ca
Danny4life <osiki.segun@gmail.com>
2025-07-05 10:27:01 +0100

    edit file

---
+++ src/main/java/SingletonEx.java
@@ -0,0 +1,15 @@
+public class SingletonEx {
+
+
+    public static void main(String[] args) {
+
+
+        SingletonEx instance1 = new SingletonEx(); // 10memory
+        SingletonEx instance2 = new SingletonEx(); //
+
+        SingletonEx instance3 = new SingletonEx();
+        SingletonEx instance4 = new SingletonEx();
+
+
+    }
+}
